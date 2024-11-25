d328a78e3ad139e2c4fcf8659b47f1787f7ffef9
dssfelipe <164665252+dssfelipe@users.noreply.github.com>
2024-11-24 21:19:32 -0300

    correções - float

--- nota-vetores/nota-vetor.c
+++ nota-vetores/nota-vetor.c
@@ -2,21 +2,23 @@
 
 int main() {
     
-    int vet[4];
-    int i=0, soma, media;
+    float vet[4];
+    float soma, media;
+    int i = 1;
+    
     
    for (i=1; i<5; i++){
-       printf("Digite a sua %d° nota: ", i);
-       scanf("%d", &vet[i]);
+       printf("Digite a sua %d� nota: ", i);
+       scanf("%f", &vet[i]);
        soma+=vet[i];
    }
    
     media = soma/4;
     
     if (media >= 6){
-        printf("\nParabéns você esta aprovado, com a media %d", media);
+        printf("\nParabéns você esta aprovado, com a media %.2f", media);
     }else{
-        printf("\nInfelizmente você esta reprovado, com a media %d", media);
+        printf("\nInfelizmente você esta reprovado, com a media %.2f", media);
     }
     
     
